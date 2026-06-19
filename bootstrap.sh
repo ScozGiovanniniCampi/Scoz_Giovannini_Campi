9451e9c7c74d834d7e816ae4a25577d0fd73df1d
davidecampi-bit <davide.campi@studenti.unitn.it>
2026-06-19 14:33:13 +0200

    Change split command to use line count instead of rows

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ FILE="$2"
 
 tail -n +2 "$FILE" > catalog0.csv
 ROW_NUMBER=$(wc -l < catalog0.csv)
-split -l $ROW_NUMBER -d --additional-suffix=".csv" catalog0.csv catalog
+split -n l/$LIB_NUMBER -d --additional-suffix=".csv" catalog0.csv catalog
 rm catalog0.csv
