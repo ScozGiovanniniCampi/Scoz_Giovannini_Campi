99aef312f6c3c101bf16ca00ab185e73e265e110
davidecampi-bit <davide.campi@studenti.unitn.it>
2026-06-19 14:42:13 +0200

    Remove ROW_NUMBER calculation from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,5 @@ LIB_NUMBER=$1
 FILE="$2"
 
 tail -n +2 "$FILE" > catalog0.csv
-ROW_NUMBER=$(wc -l < catalog0.csv)
 split -n l/$LIB_NUMBER -d --additional-suffix=".csv" catalog0.csv catalog
 rm catalog0.csv
